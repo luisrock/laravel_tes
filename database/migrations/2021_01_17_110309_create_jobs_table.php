1eaf6414f3108d36b959ddcc2bdd89feea1ed591
Mauro Lopes <mauro.lopes@descomplica.com.br>
2021-05-06 16:33:09 -0300

    back to previous

--- database/migrations/2021_01_17_110309_create_jobs_table.php
+++ database/migrations/2021_01_17_110309_create_jobs_table.php
@@ -13,17 +13,15 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('jobs')) { //deploys estava falhando dizendo que a tabela já existia...
-            Schema::create('jobs', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('queue')->index();
-                $table->longText('payload');
-                $table->unsignedTinyInteger('attempts');
-                $table->unsignedInteger('reserved_at')->nullable();
-                $table->unsignedInteger('available_at');
-                $table->unsignedInteger('created_at');
-            });    
-          }
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
     }
 
     /**
