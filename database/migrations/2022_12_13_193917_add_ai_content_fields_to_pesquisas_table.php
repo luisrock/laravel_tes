d55429044d6d0f27817424b37e643b7b835e2f90
luisrock <mauluis@gmail.com>
2022-12-13 19:53:35 -0300

    adding ai columns to pesquisas

---
+++ database/migrations/2022_12_13_193917_add_ai_content_fields_to_pesquisas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAiContentFieldsToPesquisasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pesquisas', function (Blueprint $table) {
+            //add column
+            $table->text('ai_prompt')->nullable()->after('tribunal');
+            $table->text('ai_answer')->nullable()->after('ai_prompt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pesquisas', function (Blueprint $table) {
+            //undo
+            $table->dropColumn('ai_prompt');
+            $table->dropColumn('ai_answer');
+        });
+    }
+}
