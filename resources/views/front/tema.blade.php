2daf9ebaedc58d3196284a09655fc70b22c732dd
luisrock <mauluis@gmail.com>
2023-03-25 14:33:14 -0300

    improvements on concept

--- resources/views/front/tema.blade.php
+++ resources/views/front/tema.blade.php
@@ -6,9 +6,11 @@
 
 @section('content')
 
+@auth
 @php
-$admin = false;
+    $admin = in_array(Auth::user()->email, ['mauluis@gmail.com','trator70@gmail.com','ivanaredler@gmail.com'])
 @endphp
+@endauth
 
 
 <!-- Page Content -->
@@ -30,11 +32,9 @@ $admin = false;
         </div>
         <p>
             Faça <a href="{{ route('searchpage') }}">outra pesquisa</a> ou veja as <a href="{{ route('alltemaspage') }}">pesquisas prontas</a>.
-            @auth
-            @if($admin = in_array(Auth::user()->email, ['mauluis@gmail.com','trator70@gmail.com','ivanaredler@gmail.com']))
+            @if($admin)
             <br><a href="{{ route('admin') }}">Admin</a>
             @endif
-            @endauth
         </p>
     </div>
 </div>
@@ -129,37 +129,43 @@ $admin = false;
     <!-- criar um textarea com um botao para buscar conceito via GPT-3  -->
     <div class="block block-conceito">
         <table class="table table-striped table-vcenter table-results">
-            <tbody>                                                
+            <tbody>   
+                <tr>
+                  <td>
+                    <h4 class="h5 mt-3 mb-2">
+                        <a href="#">Gerador de Conceito</a>
+                    </h4>
+                    <input type="radio" id="gpt-3.5-turbo" name="gpt-model" value="gpt-3.5-turbo" >
+                    <label for="gpt-3.5-turbo">GPT-3.5 Turbo</label>
+                    <input type="radio" id="gpt-4" name="gpt-model" value="gpt-4" checked>
+                    <label for="gpt-4">GPT-4</label>
+                  </td>
+                </tr>
                 <tr>
                     <td>
-                        <h4 class="h5 mt-3 mb-2">
-                            <a href="#">Gerador de Conceito</a>
-                        </h4>
+                      <code>System Prompt</code>
+                        <textarea id="concept-system-prompt" class="form-control" rows="3">Você é um jurista brasileiro, professor de direito e conhecedor da jurisprudência formada até 2021. Você nunca inventa e quando não sabe, diz apenas 'Desculpe'.</textarea>
+                    </td>
+                    <td>
+                      <code>User Prompt</code>
+                        <textarea id="concept-user-prompt" class="form-control" rows="3">Apresente um conceito objetivo e didático sobre o seguinte tema: {{$label}}</textarea>
+                    </td>
+                </tr>                                             
+                <tr>
+                    <td>
                         <p class="d-sm-block text-muted" id="conceito-gerado"></p>
                         <!-- <span class="text-muted" style="display: flex;justify-content: flex-end;font-size: 0.8em;">Gerado por GPT-4</span> -->
                         <!-- if admin, insert 3 buttons: validate, edit or remove  -->
                         <div id = "content-actions-buttons" style="display: flex;justify-content: flex-end; column-gap: 10px;">
                             <button type="button" class="btn btn-sm btn-success" id="concept-create" data-concept-id="{{$id}}" data-concept-label="{{$label}}" data-concept-save-route="{{ route('save-concept') }}" data-concept-generate-route="{{ route('generate-concept') }}">
                                 <span id="original-content"> 
-                                  <i class="fa fa-check"></i> Gerar com GPT-4
+                                  <i class="fa fa-check"></i> Gerar
                                 </span>
                                 <span id="loading" style="display:none;">
                                   <i class="fa fa-spinner fa-spin"></i> Aguarde...
                                 </span>
                             </button>
                         </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                      <p>System Prompt</p>
-                        <textarea id="concept-system-prompt" class="form-control" rows="5">Você é um jurista brasileiro, professor de direito e conhecedor da jurisprudência formada até 2021. Você nunca inventa e quando não sabe, diz apenas 'Realmente não sei. Desculpe.'.</textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                      <p>User Prompt</p>
-                        <textarea id="concept-user-prompt" class="form-control" rows="5">Apresente um conceito objetivo e didático sobre o seguinte tema: {{$label}}</textarea>
                     </td>
                 </tr>
             </tbody>
