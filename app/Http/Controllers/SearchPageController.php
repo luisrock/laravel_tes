f62d47bf0fe24f4c29e7cf3b32498f8befcb33a4
Mauro Lopes <mauluis@gmail.com>
2023-10-05 12:14:48 -0300

    more stj links

--- app/Http/Controllers/SearchPageController.php
+++ app/Http/Controllers/SearchPageController.php
@@ -18,15 +18,16 @@ class SearchPageController extends Controller
     {
 
         $lista_tribunais = Config::get('tes_constants.lista_tribunais');
-        $lista_tribunais_string = implode(",",array_keys($lista_tribunais));
+        $lista_tribunais_string = implode(",", array_keys($lista_tribunais));
         $display_pdf = '';
         //Initial view (no search)
-        if(empty($request->query())) {
-            return view('front.search', compact('lista_tribunais','display_pdf'));
+        if (empty($request->query())) {
+            return view('front.search', compact('lista_tribunais', 'display_pdf'));
         }
 
         //User is searching. Prepare and return results
-        $query = $request->validate([
+        $query = $request->validate(
+            [
                 //compat with extension
                 'q' => 'required_without:keyword|min:3',
                 'keyword' => 'required_without:q|min:3',
@@ -40,11 +41,11 @@ class SearchPageController extends Controller
                 'tribunal.in' => 'Por favor, indique um tribunal/órgão válido para a sua pesquisa.',
             ]
         );
-        
+
         $keyword = $query['q'] ?? $query['keyword']; //compat with extension
         $tribunal = $query['tribunal'];
         $pdf = !empty($query['print']) && 'pdf' == $query['print'];
-        $display_pdf = ($pdf) ? 'display:none;' : ''; 
+        $display_pdf = ($pdf) ? 'display:none;' : '';
         $tribunal_lower = strtolower($tribunal);
         $tribunal_upper = strtoupper($tribunal);
         $tribunal_array = $lista_tribunais[$tribunal_upper];
@@ -52,30 +53,31 @@ class SearchPageController extends Controller
         $output = [];
 
         //search in db (not through tribunal API)
-        if($lista_tribunais[$tribunal]['db']) {
+        if ($lista_tribunais[$tribunal]['db']) {
             //Getting the results by querying tes db
-            $output = tes_search_db($keyword,$tribunal_lower,$tribunal_array);    
+            $output = tes_search_db($keyword, $tribunal_lower, $tribunal_array);
         } else {
             //Getting the results by calling the tribunal API
             //tratando keyword
             $keyword = buildFinalSearchStringForApi($keyword, $tribunal_upper);
-            $output = call_request_api($tribunal_lower,$keyword);
-            
-            //dd($output);
+            $output = call_request_api($tribunal_lower, $keyword);
+
         }
+
+        // dd($output);
 
         //dd($output);
         $canonical_url = '';
 
         //If search is fruitful, save it to db in order to generate page (SEO purposes)
-        if(!empty($output['total_count']) && $output['total_count'] > 0) {
+        if (!empty($output['total_count']) && $output['total_count'] > 0) {
             SearchToDbPesquisas::dispatch($keyword);
             $canonical_url = url('/') . '/tema/' . slugify($keyword);
         }
 
         //obs: when searching by calling the tribunal API and getting 500 error, output will be a string...
-        $html = view($results_view, compact('lista_tribunais','keyword', 'tribunal', 'output', 'display_pdf', 'canonical_url'));
-        if(!$pdf) {
+        $html = view($results_view, compact('lista_tribunais', 'keyword', 'tribunal', 'output', 'display_pdf', 'canonical_url'));
+        if (!$pdf) {
             return $html;
         }
 
@@ -90,12 +92,11 @@ class SearchPageController extends Controller
         ]);
 
         $mpdf->setBasePath($url_request);
-        $mpdf->SetWatermarkText('T&S',0.05);
+        $mpdf->SetWatermarkText('T&S', 0.05);
         $mpdf->SetHeader("$keyword|{DATE d/m/Y}|{PAGENO}");
         $mpdf->SetFooter('|' . url()->current() . '|');
         $mpdf->WriteHTML($html->render());
-        $mpdf->Output('tes_'.$tribunal.'_'.$keyword.'.pdf', 'D');
-        
+        $mpdf->Output('tes_' . $tribunal . '_' . $keyword . '.pdf', 'D');
+
     } //end public function
 }
-
--- resources/views/front/results/inners/stj_rep.blade.php
+++ resources/views/front/results/inners/stj_rep.blade.php
@@ -1,31 +1,34 @@
 @foreach ($output['tese']['hits'] as $rep)
-      <tr>
+    <tr>
         <td>
-          <div style="display:flex; justify-content:space-between;"> 
-            <h4 class="h5 mt-3 mb-2">
-              <a href="{{ $rep['trib_rep_url'] }}" target="_blank"> Tema/Repetitivo {{ $rep['trib_rep_numero'] }}</a>
-            </h4>
-            <span class="text-muted mt-3 mb-2">
-              {{ $rep['trib_rep_orgao'] }}
+            <div style="display:flex; justify-content:space-between;">
+                <h4 class="h5 mt-3 mb-2">
+                    <a href="{{ $rep['trib_rep_url'] }}" target="_blank"> Tema/Repetitivo
+                        {{ $rep['trib_rep_numero'] }}</a>
+                </h4>
+                <span class="text-muted mt-3 mb-2">
+                    {{ $rep['trib_rep_orgao'] }}
+                </span>
+            </div>
+            <p class="d-sm-block text-muted">
+                QUESTÃO: {{ $rep['trib_rep_tema'] }}
+            </p>
+            <p class="d-sm-block" style="font-weight: bold;">
+                {{ $rep['trib_rep_tese'] }}
+            </p>
+            <span class="text-muted" style="display: flex;justify-content: flex-end;font-size: 0.8em;">
+                Situação: {{ $rep['trib_rep_situacao'] }}
+                (última verificação em {{ $rep['trib_rep_data'] }})
             </span>
-          </div>
-          <p class="d-sm-block text-muted">
-            QUESTÃO: {{ $rep['trib_rep_tema'] }}
-          </p>
-          <p class="d-sm-block" style="font-weight: bold;">
-            {{ $rep['trib_rep_tese'] }}
-          </p>
-          <span class="text-muted" style="display: flex;justify-content: flex-end;font-size: 0.8em;">
-            Situação: {{ $rep['trib_rep_situacao'] }} 
-            (última verificação em {{ $rep['trib_rep_data'] }})
-          </span>
-          <span class="tes-clear tes-text-to-be-copied" style="display: none">TEMA {{ $rep['trib_rep_numero'] }} ({{ $rep['trib_rep_orgao'] }}): {{ $rep['trib_rep_tema'] }} TESE: {{ $rep['trib_rep_tese'] }} SITUAÇÃO: {{ $rep['trib_rep_situacao'] }}</span>
-          <button class="btn btn-rounded btn-outline-primary btn-sm mr-1 mb-3 btn-copy-text">
-            <span>
-              <i class="fa fa-copy"></i>
-            </span>
-          </button>
+            <span class="tes-clear tes-text-to-be-copied" style="display: none">TEMA {{ $rep['trib_rep_numero'] }}
+                ({{ $rep['trib_rep_orgao'] }}): {{ $rep['trib_rep_tema'] }} TESE: {{ $rep['trib_rep_tese'] }} SITUAÇÃO:
+                {{ $rep['trib_rep_situacao'] }}</span>
+            <button class="btn btn-rounded btn-outline-primary btn-sm mr-1 mb-3 btn-copy-text">
+                <span>
+                    <i class="fa fa-copy"></i>
+                </span>
+            </button>
 
         </td>
-      </tr>
+    </tr>
 @endforeach
